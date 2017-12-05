8a5da4671798292e24b49dd10d8e5319081c7f71
Scandinaf_ <sergeyqwertyborovskiy@gmail.com>
2017-12-05 21:18:07 +0300

    Initial Commit

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "SportEventsAnalyzer"
+
+version := "1.0"
+
+scalaVersion := "2.12.4"
---
+++ src/main/scala/Main.scala
@@ -0,0 +1,10 @@
+/**
+  * Created by serge on 20.11.2017.
+  */
+
+object Main {
+
+  def main(args: Array[String]): Unit = {
+    println("Hello World")
+  }
+}
