33478244d64512a38f51c5811cc1e44877b39e1d
Scandinaf_ <sergeyqwertyborovskiy@gmail.com>
2017-12-05 22:48:28 +0300

    Tests were added

---
+++ src/test/scala/service/ConfigSpec.scala
@@ -0,0 +1,16 @@
+package service
+
+/**
+  * Created by serge on 05.12.2017.
+  */
+
+import org.scalatest.FlatSpec
+
+class ConfigSpec extends FlatSpec {
+
+  behavior of "Config"
+
+  "Websites block" should "be non-empty" in {
+    assert(!Config.websites.isEmpty)
+  }
+}
