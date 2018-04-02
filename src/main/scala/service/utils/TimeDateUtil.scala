0abddee3b479c995b6daa01e6f6d7fb7fcd847bb
Scandinaf_ <sergeyqwertyborovskiy@gmail.com>
2018-04-02 23:09:07 +0300

    The date lower bound was added

--- src/main/scala/service/utils/TimeDateUtil.scala
+++ src/main/scala/service/utils/TimeDateUtil.scala
@@ -7,21 +7,24 @@ import service.utils.ImplicitHelper.CalendarImplicits.CalendarBuilder
 object TimeDateUtil {
   def checkDateInRange[T](l: Vector[T], range: DateRange, f: T => Date) = {
     val d = getDateByRange(range)
-    l.filter(v => f(v).before(d))
+    l.filter(v => d.check(f(v)))
   }
 
   def checkDateInRange(date: Date, range: DateRange) =
-    date.before(getDateByRange(range))
+    getDateByRange(range).check(date)
 
   private def getDateByRange(range: DateRange) = range match {
     case DayRange  => getDate(Calendar.DAY_OF_WEEK, 2)
     case WeekRange => getDate(Calendar.DAY_OF_WEEK, 8)
   }
 
-  private def getDate(field: Int, amount: Int) =
-    setTimeBeginningDay(Calendar.getInstance)
-      .addB(field, amount)
-      .getTime
+  private def getDate(field: Int, amount: Int) = {
+    val c = Calendar.getInstance()
+    new DateRangeHelper(c.getTime,
+                        setTimeBeginningDay(c)
+                          .addB(field, amount)
+                          .getTime)
+  }
 
   private def setTimeBeginningDay(c: Calendar) =
     c.setB(Calendar.HOUR_OF_DAY, 0)
@@ -31,4 +34,8 @@ object TimeDateUtil {
   sealed trait DateRange
   object DayRange extends DateRange
   object WeekRange extends DateRange
+
+  private class DateRangeHelper(startDate: Date, endDate: Date) {
+    def check(date: Date) = date.after(startDate) && date.before(endDate)
+  }
 }
